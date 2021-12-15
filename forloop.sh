8f31d4203b61695b7faed9466b5f1cb4c55a7d60
Nathanho <nho@ucsb.edu>
2021-12-15 12:37:40 -0800

    Add notes about the forloop

---
+++ forloop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cards=(1,2,3,4,5,6,7,8,9,10,10,10)
+echo "how many cards would you like?"
+read value
+for i in $(seq 1 value);
+do echo ${cards[RANDOM%${#cards{@]}]};
+done
+#for loop iterates over value given by user
+
